607660ef72128a93db5761ac731a5740ed4a2c5b
Paulina Grunwald <paulina.grunwald@gmail.com>
2018-05-24 15:42:22 +0200

    update main.js
    add javascript quote to animate the main quote on the website

--- pre-academy-homework/js/main.js
+++ pre-academy-homework/js/main.js
@@ -1,5 +1,6 @@
 
 $(document).ready(function(){
+    // smooth scrolling to various sections of the website
     $('a[href^="#"]').on('click',function (e) {
         e.preventDefault();
 
@@ -23,4 +24,26 @@ $(document).ready(function(){
             //window.location.hash = target;
         });
     });
-});
+
+    // Animate the main quote on the website
+    $('.quote').each(function(){
+      $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+    });
+
+    anime.timeline({loop: true})
+      .add({
+        targets: '.quote .letter',
+        opacity: [0,1],
+        easing: "easeInOutQuad",
+        duration: 2250,
+        delay: function(el, i) {
+          return 150 * (i+1)
+        }
+      }).add({
+        targets: '.quote',
+        opacity: 0,
+        duration: 1000,
+        easing: "easeOutExpo",
+        delay: 1000
+      });
+    });
