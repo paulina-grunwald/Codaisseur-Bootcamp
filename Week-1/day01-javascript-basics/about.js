5d9eb7b6892956702e412efbceb457a4c2b9c805
Paulina Grunwald <paulina.grunwald@gmail.com>
2018-05-28 15:47:10 +0200

    add about.js

---
+++ Week-1/day01-javascript-basics/about.js
@@ -0,0 +1,7 @@
+var myName = "Paulina";
+var age = "30";
+var birthYear = 1987;
+var message = `My name is ${myName} and I am ${age}`;
+
+console.log(message);
+console.log(`I was born in ${birthYear}. I am ${new Date().getFullYear() - birthYear} years old.`);
