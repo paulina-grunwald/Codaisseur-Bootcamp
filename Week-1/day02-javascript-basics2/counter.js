b8403b791b8e8dc733cab039b4033649bcd844d1
Paulina Grunwald <paulina.grunwald@gmail.com>
2018-05-29 11:15:20 +0200

    add folder

---
+++ Week-1/day02-javascript-basics2/counter.js
@@ -0,0 +1,11 @@
+/*
+Create a program that starts counting at 5. It counts up to (including) 50, 
+and count only every fifth number. Use a while loop to build it
+
+*/
+
+
+
+for (counter=5; counter < 50; counter+=5) {
+  console.log(counter);
+};
