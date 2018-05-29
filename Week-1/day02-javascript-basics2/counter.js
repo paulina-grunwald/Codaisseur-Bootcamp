52ac12642ce89b8f5b7c57572288e70ebfb926d9
Paulina Grunwald <paulina.grunwald@gmail.com>
2018-05-29 11:20:39 +0200

    update exercise

--- Week-1/day02-javascript-basics2/counter.js
+++ Week-1/day02-javascript-basics2/counter.js
@@ -9,3 +9,9 @@ and count only every fifth number. Use a while loop to build it
 for (counter=5; counter < 50; counter+=5) {
   console.log(counter);
 };
+
+
+let counter2 = 5;
+while(counter2 < 50) {
+  console.log(counter2+=5);
+}
