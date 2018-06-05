734e4e3a6c76fb05d2ec09edf0e7208bb53ac8a8
Paulina Grunwald <paulina.grunwald@gmail.com>
2018-06-05 13:55:01 +0200

    add script written in ES6 that calculates score

--- Week-2/day07-Algorithms/calculateScore.js
+++ Week-2/day07-Algorithms/calculateScore.js
@@ -1,7 +1,18 @@
-cont outOfTen = (score, totalScore) => {
-    return score/totalScore*10
-}
+let quizData = [{score:16, totalScore:20},{score: 15, totalScore: 21}]
 
+const output = [8,6,2,5]
+
+const outOfTen = (quizScores) => {
+    let result=[]
+    result = quizScores.map(score => { return score.score/score.totalScore*10})
+    return console.log(result)
+}
+outOfTen(quizData)
+/*
 const itWorksforSixteenAndTwenty = (score, totalScore) => {return 8}
 
-console.log('It works for 16 and 20', itWorksforSixteenAndTwenty())l
+const itWorksWithArray = (quizData, output) => {}
+
+console.log('It works for 16 and 20', itWorksforSixteenAndTwenty())
+console.log('It works for an array', itWorksWithArray(quizScore, output))
+*/
