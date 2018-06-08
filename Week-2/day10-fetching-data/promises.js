c9ae044a79a84452ccee30095a76eae262d8e591
Paulina Grunwald <paulina.grunwald@gmail.com>
2018-06-08 16:27:30 +0200

    update exercise files

---
+++ Week-2/day10-fetching-data/promises.js
@@ -0,0 +1,40 @@
+const documents = {
+    1: {content: "I am a document", authorId: 1},
+    2: {content: "I am also a document", authorId: 2},
+    3: {content: "You guessed it!", authorId: 2}
+ }
+ const authors = {
+    1: {name: "Adam"},
+    2: {name: "Dave"}
+ }
+ 
+ 
+ const getDocument = (documentId) => {
+  return new Promise((resolve, reject) => {
+    function loadComplete(document) {
+      if (documents[documentId] === undefined) {
+        reject('Document not found')
+      }
+      resolve(documents[documentId])
+    }
+    setTimeout(loadComplete, 1000)
+  })
+ }
+ 
+ const getAuthor = (authorId) => {
+  //console.log(authorId);
+  return new Promise((resolve, reject) => {
+    function loadComplete(author) {
+      if (authors[authorId] === undefined) {
+        reject('Author not found')
+      }
+      resolve(authors[authorId])
+    }
+    setTimeout(loadComplete, 1000)
+  })
+ }
+ 
+ getDocument(2)
+  .then(document => getAuthor(document.authorId))
+  .then(author => console.log(author))
+  .catch(error => console.log(error))
---
+++ Week-2/day10-fetching-data/request.js
@@ -0,0 +1,17 @@
+const request = new XMLHttpRequest()
+request.addEventListener("load", function() {
+  document.body.innerHTML =
+    "<pre>" +
+    JSON.stringify(JSON.parse(this.responseText), null, " ") +
+    "</pre>"
+})
+request.open("GET", "https://swapi.co/api/people/1/")
+request.send()
+
+
+fetch("https://swapi.co/api/people/2/")
+  .then(response => response.json())
+  .then(myJson => {
+    document.body.innerHTML =
+      "<pre>" + JSON.stringify(myJson, null, " ") + "</pre>"
+  })
